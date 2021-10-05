7963072a7654a7fdcf0806df99adf3706b6e304f
Sumit Saha <47948885+SUMIT-RIJU@users.noreply.github.com>
2021-10-05 21:40:25 +0530

    Create Binarysearch.py

---
+++ Binarysearch.py
@@ -0,0 +1,31 @@
+def binarySearch(arr,beg,end,item):  
+    if end >= beg:  
+        mid = int((beg+end)/2)  
+        if arr[mid] == item :  
+            return mid+1  
+        elif arr[mid] < item :   
+            return binarySearch(arr,mid+1,end,item)  
+        else:   
+            return binarySearch(arr,beg,mid-1,item)  
+    return -1  
+      
+  
+arr=[16, 19, 20, 23, 45, 56, 78, 90, 96, 100];  
+item = int(input("Enter the item which you want to search ?"))  
+location = -1;   
+location = binarySearch(arr,0,9,item);  
+if location != -1:   
+    print("Item found at location %d" %(location))  
+else:   
+    print("Item not found")                                                                                                                                                                              Output:
+
+
+
+//output:
+Enter the item which you want to search ? 
+96 
+Item found at location 9 
+Enter the item which you want to search ? 
+101 
+Item not found
+//
